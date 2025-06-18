c117325f571b2f018bbb27048bf7d50ad90e71de
eveper13 <evelynperez4@gmail.com>
2025-06-18 13:01:49 -0400

    Corrige ruta de main.py y Procfile

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-import os
-db_url = os.getenv("DATABASE_URL")
-
 from fastapi import FastAPI
-from app.api.endpoints import router
 
 app = FastAPI()
-app.include_router(router)
+
+@app.get("/")
+def root():
+    return {"message": "Hola desde Render!"}
