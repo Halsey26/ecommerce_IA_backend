cca8cdc0a21defb7a8213c975325b3a4a8178f24
eveper13 <evelynperez4@gmail.com>
2025-06-18 15:42:27 -0400

    end

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -7,9 +7,14 @@ router = APIRouter()
 class Log(BaseModel):
     user_id: str
     message: str
-    timestamp: datetime = datetime.utcnow()
+
+from typing import Optional
+
+timestamp: Optional[str] = None
+
 
 @router.post("/logs/")
 async def receive_log(log: Log):
+    log.timestamp = log.timestamp or datetime.utcnow().isoformat()
     print(f"📨 Log recibido: {log}")
     return {"status": "ok", "log": log}
